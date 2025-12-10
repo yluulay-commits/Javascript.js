bfe3400f9fc5ade8d9bc4d80a24b5131ab323767
yluulay-commits <yluulay@student.refactory.academy>
2025-12-10 20:16:18 +0300

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,46 @@
+
+/ / This is a single-line Comment   
+/*
+This is a multi-line commment
+*/
+Javascript's 5 primitive date types:
+1. String 
+2.Number 
+3.Boolean 
+4.Null
+5.Undefined
+Plus one extra common:Ojbect(not primitive but essential)
+// Date types examples 
+let mystring = "Hello Javascript";   //
+String
+let mynumber = 22;   //
+Number
+let myboolean = true ;   //
+Boolean
+let mynull = null ;  //
+Null
+let myundefined ;   //
+Undefined
+//Object example 
+let myobject = [ name : "Luulay" ];
+Perform Type Operations 
+console.log (typeof myString);  //
+"string"       //
+console.log (typeof myNumber);  //
+console.log(typeof myBoolean);   //
+"boolean"    //
+console.log(typeof myNull); //
+"object" (Javascript quirk)  //
+console.log(typeof myUndefined);   //
+"undefined"    //
+console.log(tyoeof myObject);   //
+"object"     //
+Simple operations:
+console.log(myNumber + 10);  //
+Addition   
+console.log(myString + "World");  //
+String concatenation 
+console.log(!myBoolean);  //
+Boolean negation 
+Display all 
+console.log9"All variables: " , myString , myNumber , myBoolean , myNuul , myUndefined , myObject);
